a26da244a8353b41e634ec04b37e44e20a269607
Bruno Carrijo <bruno.carrijo1009@gmail.com>
2021-11-01 18:40:01 -0300

    Acrescentada msg de Bom Dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -7,6 +7,8 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia!");
+            Console.ReadLine();
         }
     }
 }
